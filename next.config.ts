c326d312b90dbf6e9144d265eecdf9bd88e1428d
Rohan Rai <rohan.rai932@outlook.com>
2026-02-20 21:30:31 +0530

    bad choice - rolling back
    Signed-off-by: Rohan Rai <rohan.rai932@outlook.com>

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,5 @@
 import type { NextConfig } from "next";
 
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-inline' 'unsafe-eval' *.adobe.com *.adobedtm.com *.omtrdc.net;
-    connect-src 'self' *.adobe.com *.omtrdc.net *.demdex.net;
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' *.adobe.com *.omtrdc.net data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
-`
-  .replace(/\s{2,}/g, " ")
-  .trim();
-
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
@@ -25,19 +9,6 @@ const nextConfig: NextConfig = {
         hostname: "**",
       },
     ],
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: cspHeader,
-          },
-        ],
-      },
-    ];
   },
   allowedDevOrigins: process.env.LOCALDEVURL
     ? JSON.parse(process.env.LOCALDEVURL)
